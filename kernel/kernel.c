19828f902cf53613cc70e477d70890de08cf82d7
alexeyqian <alexeyqian@gmail.com>
2020-04-29 23:34:03 +0700

    add keyboard irq functions

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,24 @@
 #include "../drivers/screen.h"
+#include "system.h"
 
 void kmain(){
    
    clear_screen();
    print("minos\n");
    print("kernel v0.1\n");
+
+   idt_install();
+   isrs_install();
+   irq_install();
+
+   __asm__ __volatime__("sti");
+
+   timer_install();
+   keyboard_install();
+
+   print("wait started.\n");
+   timer_wait(18*2); // 2 seconds wait
+   print("wait ended.\n");
+
+   while(1);
 }
