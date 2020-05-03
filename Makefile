85c713397f5dd8e1f4086dcbcb7b1781d165751d
alexeyqian <alexeyqian@gmail.com>
2020-05-03 17:37:48 +0700

    update file structure

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ $(info $$OBJ is [${OBJ}])
 
 all: os-image
 
+run: all
+	"/mnt/c/Program FIles/Bochs-2.6.11/bochs.exe"
+
 # This is the actual disk image that the computer loads
 # which is the combination of our compiled bootsector and kernel
 os-image: boot/boot_sect.bin kernel/kernel.bin
@@ -14,7 +17,7 @@ os-image: boot/boot_sect.bin kernel/kernel.bin
 boot/boot_sect.bin: boot/boot_sect.asm
 	nasm $< -f bin -I 'boot/' -o $@
 
-kernel/kernel.bin: kernel/kernel_entry.o $(OBJ) drivers/screen.o
+kernel/kernel.bin: kernel/kernel_entry.o kernel/kernel.o $(OBJ) 
 	ld -m elf_i386 -o $@ -Ttext 0x1000 $^ --oformat binary
 
 kernel/kernel_entry.o: kernel/kernel_entry.asm $(OBJ)
--- boot/boot_sect.asm
+++ boot/boot_sect.asm
@@ -1,51 +1,30 @@
 [org 0x7c00]
 
 KERNEL_OFFSET equ 0x1000
-mov [BOOT_DRIVE], dl      ; BIOS stores our boot drive in DL, so it'si
-                          ; best to remember this for later.
+KERNEL_STACK_OFFSET equ 0x90000
 
 mov bp, 0x9000            ; Here we set out stack safely away from occupied memory
 mov sp, bp
 
-call print_new_line
-mov bx, MSG_REAL_MODE
-call print_string
-call print_new_line
+mov [BOOT_DRIVE], dl      ; BIOS stores our boot drive in DL, 
+                          ; so it's best to remember this for later.
 
-call load_kernel
+call rmprint_nl
+mov bx, MSG_REAL_MODE
+call rmprint
+call rmprint_nl
+
+call load_kernel_from_disk
 call switch_to_pm        ; Note that we never return from here
 
 jmp $
 
-[bits 16]
-load_kernel:
-    mov bx, MSG_LOAD_KERNEL
-    call print_string
-    call print_new_line
-
-    ; Set-up parameters for our disk_load routine , so 
-    ; that we load the first 15 sectors (excluding 
-    ; the boot sector) from the boot disk (i.e. our 
-    ; kernel code) to address KERNEL_OFFSET
-
-    mov bx, KERNEL_OFFSET
-    mov dh, 15                ; Load 15 sectors to ES(0x0000):BX
-    mov dl, [BOOT_DRIVE]
-    call disk_load
-    ret
-
-%include "print_string.asm"
-%include "disk_load.asm"
-%include "gdt.asm"
-%include "switch_to_pm.asm"
-%include "print_string_pm.asm"
-
-[bits 32]
-BEGIN_PM:
-    mov ebx, MSG_PROT_MODE
-    call print_string_pm
-    call KERNEL_OFFSET
-    jmp $
+%include "rmprint.inc"
+%include "disk_load.inc"
+%include "load_kernel_from_disk.inc"
+%include "gdt.inc"
+%include "switch_to_pm.inc"
+%include "pmprint.inc"
 
 ; Global variables
 BOOT_DRIVE:
