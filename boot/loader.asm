e362b568ae9a7177e06e8cc87a3cab2bef16b355
alexeyqian <alexeyqian@gmail.com>
2020-07-02 17:23:00 +0700

    fix memory mapping issue

--- boot/loader.asm
+++ boot/loader.asm
@@ -22,12 +22,6 @@ PAGE_TABLE_BASE equ 0x101000 ; 1M + 4K
 
 ; ================== entry point =================
 jmp short start       ; fixed position, start execute from first line of this bin file after loading by boot.
-
-%include "fixed_bios_parameter_block.inc" ; data
-%include "pm.inc" ; constants and macros
-%include "multiboot_info.inc"
-%include "rm_gdt.inc" ; data
-
 start:
     mov ax, cs
     mov ds, ax
@@ -37,25 +31,18 @@ start:
        
     mov bx, msg_in_loader
     call rm_print_str
-	
+
+	call rm_get_memory_map
 	call rm_load_kernel_bin
 
     ; prepare for proteded mode
-	
 	lgdt [gdt_ptr]
-	
 	; enable A20
 	cli
 	in al, 0x92 ; enable A20
 	or al, 0b00000010
 	out 0x92, al
-
-	; after enable A20, use bios to read memory size and map
-	; before A20 is enabled, system cannot get memory over 1M
-	call rm_get_memory_map
-	; TODO: fill kernel info struct to pass as parameters to kernel
-	; TODO: call rm_fill_boot_info
-
+	; enable protected mode
 	mov eax, cr0
 	or eax, 1
 	mov cr0, eax
@@ -64,8 +51,11 @@ start:
 	jmp dword code_selector: (LOADER_PHYSICAL_ADDR + pm_start)
     jmp $
 
-%include "rm_load_kernel_bin.inc"
+%include "pm.inc" ; constants and macros
+%include "fixed_bios_parameter_block.inc" ; data
+%include "rm_gdt.inc" ; data
 %include "rm_get_memory_map.inc"
+%include "rm_load_kernel_bin.inc"
 %include "rm_lib.inc"
 %include "rm_read_sectors.inc"
 %include "rm_get_fat_entry.inc"
@@ -82,36 +72,9 @@ sector_num:             dw 0
 kernel_size:            dd 0
 root_dir_sectors_for_loop: dw ROOT_DIR_SECTORS
 
-boot_info:
-istruc multiboot_info
-	at multiboot_info.flags,			dd 0
-	at multiboot_info.memoryLo,			dd 0
-	at multiboot_info.memoryHi,			dd 0
-	at multiboot_info.bootDevice,		dd 0
-	at multiboot_info.cmdLine,			dd 0
-	at multiboot_info.mods_count,		dd 0
-	at multiboot_info.mods_addr,		dd 0
-	at multiboot_info.syms0,			dd 0
-	at multiboot_info.syms1,			dd 0
-	at multiboot_info.syms2,			dd 0
-	at multiboot_info.mmap_length,		dd 0
-	at multiboot_info.mmap_addr,		dd 0
-	at multiboot_info.drives_length,	dd 0
-	at multiboot_info.drives_addr,		dd 0
-	at multiboot_info.config_table,		dd 0
-	at multiboot_info.bootloader_name,	dd 0
-	at multiboot_info.apm_table,		dd 0
-	at multiboot_info.vbe_control_info,	dd 0
-	at multiboot_info.vbe_mode_info,	dw 0
-	at multiboot_info.vbe_interface_seg,dw 0
-	at multiboot_info.vbe_interface_off,dw 0
-	at multiboot_info.vbe_interface_len,dw 0
-iend
-
 ;=================================================
 ; bits 32 all below code running in protected mode
 ;=================================================
-;[section .text]
 [bits 32]
 pm_start: ; entry point for protected mode
 	mov ax, video_selector
@@ -134,7 +97,7 @@ pm_start: ; entry point for protected mode
 	call pm_print_mem_ranges	
 	
 	call pm_setup_paging
-
+	
 	push pm_paging_enabled_str
 	call pm_print_str	
 	add esp, 4
